81015ae889fe6c0093891402e9af865586aa34ec
Leonardo Scanavacca Falango <leonardo.falango@pucpr.edu.br>
2023-03-23 21:21:25 -0300

    Add: MapToReduce and Reduce class

--- src/basic/WordCount.java
+++ src/basic/WordCount.java
@@ -32,20 +32,51 @@ public class WordCount {
         // criacao do job e seu nome
         Job j = new Job(c, "wordcount");
 
+        // registro de classes
+        j.setJarByClass(WordCount.class);
+        j.setMapperClass(MapForWordCount.class);
+        j.setReducerClass(ReduceForWordCount.class);
+
+        // tipos de saida
+        j.setMapOutputKeyClass(Text.class);
+        j.setMapOutputValueClass(IntWritable.class);
+        j.setMapOutputKeyClass(Text.class);
+        j.setMapOutputValueClass(IntWritable.class);
+
+        // definindo arquivos de entrada e saida
+        FileInputFormat.addInputPath(j, input);
+        FileOutputFormat.setOutputPath(j, output);
+
+        // rodar
+        j.waitForCompletion(false);
     }
 
     public static class MapForWordCount extends Mapper<LongWritable, Text, Text, IntWritable> {
 
         public void map(LongWritable key, Text value, Context con)
                 throws IOException, InterruptedException {
+            // value é cada linha do arquivo
+            String line = value.toString();
+            String[] words = line.split(" ");
+
+            // Gerar a chave valor de cada palavra
+            for (String element : words) {
+                Text keyElem = new Text(element);
+                IntWritable val = new IntWritable(1);
+                con.write(keyElem, val);
+            }
         }
     }
 
     public static class ReduceForWordCount extends Reducer<Text, IntWritable, Text, IntWritable> {
-
         public void reduce(Text key, Iterable<IntWritable> values, Context con)
                 throws IOException, InterruptedException {
+            int soma = 0;
+            for (IntWritable v : values) {
+                soma += v.get();
+            }
 
+            con.write(key, new IntWritable(soma));
         }
     }
 
